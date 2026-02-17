f2be5fdf30517d95f16b94ab601771ba3ef275c2
Egor Bessonov <bessonoven@mail.ru>
2026-02-17 16:08:14 +0300

    Refactor User`s domain structs

--- internal/dto/user/create_user.go
+++ internal/dto/user/create_user.go
@@ -11,5 +11,6 @@ type CreateUserRequest struct {
 type CreateUserResponse struct {
 	Id        int64     `json:"id" validate:"required"`
 	Username  string    `json:"username" validate:"required,min=3,max=30"`
+	APIKey    string    `json:"api_key"`
 	CreatedAt time.Time `json:"created_at" validate:"required"`
 }
